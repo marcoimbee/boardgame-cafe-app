6bf8bd5e9c4fb2a7bc954e5771b9c97a8dfa6c75
g.sferr <94866345+g-sferr@users.noreply.github.com>
2023-10-26 00:13:48 +0200

    minor fix on Interface Mongo Class

--- src/main/java/it/unipi/dii/lsmsdb/boardgamecafe/repository/mongodbms/INFBoardgameMongoDB.java
+++ src/main/java/it/unipi/dii/lsmsdb/boardgamecafe/repository/mongodbms/INFBoardgameMongoDB.java
@@ -1,6 +1,7 @@
 package it.unipi.dii.lsmsdb.boardgamecafe.repository.mongodbms;
 
 //import it.unipi.dii.lsmsdb.phoneworld.model.Phone;
+import it.unipi.dii.lsmsdb.boardgamecafe.mvc.model.Boardgame;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
@@ -10,29 +11,29 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface INFBoardgameMongoDB extends MongoRepository<Phone, String> {
+public interface INFBoardgameMongoDB extends MongoRepository<Boardgame, String> {
 
-    List<Phone> findByReleaseYear(int releaseYear);
-    List<Phone> findByBrandOrderByReleaseYearDesc(String brand);
+    List<Boardgame> findByReleaseYear(int releaseYear);
+    List<Boardgame> findByBrandOrderByReleaseYearDesc(String brand);
 
     @Query(value = "{'name': {$regex : ?0, $options: 'i'}}")
-    List<Phone> findByNameRegexOrderByReleaseYearDesc(String name, Sort sort);
+    List<Boardgame> findByNameRegexOrderByReleaseYearDesc(String name, Sort sort);
 
     @Query(value = "{'ram': {$regex : /.*?0.*GB/, $options: 'i'}}")
-    List<Phone> findByRamRegexOrderByReleaseYearDesc(String ram, Sort sort);
+    List<Boardgame> findByRamRegexOrderByReleaseYearDesc(String ram, Sort sort);
 
     @Query(value = "{'storage': {$regex : /^?0GB.*/, $options: 'i'}}")
-    List<Phone> findByStorageRegexOrderByReleaseYearDesc(String storage, Sort sort);
+    List<Boardgame> findByStorageRegexOrderByReleaseYearDesc(String storage, Sort sort);
 
     @Query(value = "{'chipset': {$regex : ?0, $options: 'i'}}")
-    List<Phone> findByChipsetRegexOrderByReleaseYearDesc(String chipset, Sort sort);
+    List<Boardgame> findByChipsetRegexOrderByReleaseYearDesc(String chipset, Sort sort);
 
     @Query(value = "{'batterySize': {$regex : /^?0 mAh.*/, $options: 'i'}}")
-    List<Phone> findByBatterySizeRegexOrderByReleaseYearDesc(String batterySize, Sort sort);
+    List<Boardgame> findByBatterySizeRegexOrderByReleaseYearDesc(String batterySize, Sort sort);
 
     @Query(value = "{'cameraPixels': {$regex : /^?0 MP.*/, $options: 'i'}}")
-    List<Phone> findByCameraPixelsRegexOrderByReleaseYearDesc(String cameraPixels, Sort sort);
+    List<Boardgame> findByCameraPixelsRegexOrderByReleaseYearDesc(String cameraPixels, Sort sort);
 
-    Optional<Phone> findByName(String name);
+    Optional<Boardgame> findByName(String name);
 
 }
