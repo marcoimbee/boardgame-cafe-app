cb05e63c530d152b84e32934eb6a86d196cf32c8
g.sferr <94866345+g-sferr@users.noreply.github.com>
2023-11-25 18:23:47 +0100

    main class updated for test on BoardgameCafeApp Databases (Neo4j & Mongo)

--- src/main/java/it/unipi/dii/lsmsdb/boardgamecafe/BoardgamecafeApplication.java
+++ src/main/java/it/unipi/dii/lsmsdb/boardgamecafe/BoardgamecafeApplication.java
@@ -5,51 +5,38 @@ package it.unipi.dii.lsmsdb.boardgamecafe;
 //import it.unipi.dii.lsmsdb.phoneworld.repository.neo4j.PhoneNeo4j;
 //import it.unipi.dii.lsmsdb.phoneworld.repository.neo4j.UserNeo4j;
 
-import it.unipi.dii.lsmsdb.boardgamecafe.mvc.model.UserTest;
-import it.unipi.dii.lsmsdb.boardgamecafe.mvc.model.ModelBean;
-import it.unipi.dii.lsmsdb.boardgamecafe.repository.neo4jdbms.GraphNeo4jDB;
-import it.unipi.dii.lsmsdb.boardgamecafe.repository.neo4jdbms.UserNeo4jDB;
+import it.unipi.dii.lsmsdb.boardgamecafe.mvc.model.mongo.UserTest;
+import it.unipi.dii.lsmsdb.boardgamecafe.mvc.model.neo4j.UserNeo4j;
+import it.unipi.dii.lsmsdb.boardgamecafe.repository.neo4jdbms.INFUserNeo4jDB;
 import it.unipi.dii.lsmsdb.boardgamecafe.repository.mongodbms.INFUserMongoDB;
-import it.unipi.dii.lsmsdb.boardgamecafe.repository.mongodbms.UserMongo;
 import it.unipi.dii.lsmsdb.boardgamecafe.services.ServiceUser;
 
 //import it.unipi.dii.lsmsdb.boardgamecafe.mvc.view.FxmlView;
 //import it.unipi.dii.lsmsdb.boardgamecafe.mvc.view.StageManager;
-
 //import javafx.application.Application;
 //import javafx.stage.Stage;
+
+import org.neo4j.driver.Driver;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.event.EventListener;
+
+import java.util.Optional;
 
 @SpringBootApplication  //extends Application
 public class BoardgamecafeApplication {
 
     @Autowired
+    Driver driver;
+    @Autowired
     private INFUserMongoDB mongoRepository;
     @Autowired
+    private INFUserNeo4jDB neo4jRepository;
+    @Autowired
     private ServiceUser serviceUser;
-
-    private static final BoardgamecafeApplication singleton = new BoardgamecafeApplication();
-    private ModelBean modelBean = new ModelBean();
-    private GraphNeo4jDB graphNeo4j = new GraphNeo4jDB("bolt://localhost:7687",
-                                                        "neo4j", "PhoneWorld");
-    private UserNeo4jDB userNeo4j = new UserNeo4jDB(graphNeo4j);
-
-    public static BoardgamecafeApplication getInstance() {
-        return singleton;
-    }
-    public ModelBean getModelBean() {
-        return modelBean;
-    }
-    public UserNeo4jDB getUserNeo4j() {
-        return userNeo4j;
-    }
-
 
     public static void main(String[] args)
     {
@@ -61,37 +48,48 @@ public class BoardgamecafeApplication {
     @EventListener(ApplicationReadyEvent.class)
     public void afterTheStart()
     {
-        /* ------ Test Mongo Operations on BoardGameCafeDB ------*/
+        /* ------ Test Operations on BoardGameCafeDB ------*/
 
         String username1 = "whitekoala768";
         String username2 = "heavyladybug904";
         String idUser = "e076a482ec7643c0a9f01db0";
 
-        UserTest user = serviceUser.createUser(idUser,
+        //UserTest userMongo = new UserTest();
+
+        /*
+        UserTest userMongo = serviceUser.createUser(idUser,
                 "heavyladybug904", "dyon.zonnenberg@example.com",
                 "123456","Dyon","Zonnenberg","male",
                 "NL","NotBanned",1974,06,11);
 
+        UserNeo4j userNeo4j = new UserNeo4j(userMongo.getId(), userMongo.getUsername());
+
         System.out.println(" \n- New user added within MongoDB -\n");
-        serviceUser.insertUser(user);
-
-        // - MongoDB Operations Management -
-        System.out.println(" \n- Shown below are all users within MongoDB -\n");
-        mongoRepository.findAll().forEach(System.out::println);
-
-        System.out.println("\n- Shown below is a specifc user into MongoDB filtered out by username -");
-        System.out.println("- Reference USERNAME: " + username2 + "\n");
-        System.out.println(mongoRepository.findByUsername(username1));
-
-        /*
-        System.out.println("\n- Shown below is a specifc user into MongoDB filtered out by username -");
-        System.out.println("- Reference USERNAME: " + username1 + "\n");
-        System.out.println(mongoRepository.findByUsername(username1));
+        serviceUser.insertUser(userMongo, userNeo4j);
         */
 
+        // - MongoDB Operations Management -
+
+        //System.out.println(" \n- Shown below are users within MongoDB -\n");
+        //mongoRepository.findAll().forEach(System.out::println);
+        //System.out.println(mongoRepository.findByUsername(username1));
+
+        //System.out.println("\n- Shown below is a specifc user into MongoDB filtered out by username -");
+        //System.out.println("- Reference USERNAME: " + username2 + "\n");
+        //System.out.println(mongoRepository.findByUsername(username1));
+
+
         // - Neo4jDB Operations Management -
-        System.out.println("\n- Shown below is a specifc user into Neo4jDB filtered out by ID -");
-        System.out.println("- Reference ID: " + idUser + "\n");
-        System.out.println(userNeo4j.findUserById(idUser));
+
+        //System.out.println(" \n- Shown below is one user within Neo4jDB -\n");
+        //System.out.println(neo4jRepository.findByUsername(username1).stream().map(u->u.getUsername()).toList());
+
+        System.out.println(" \n- Shown below are users within Neo4jDB (half of those) -\n");
+        try (var session = driver.session()){
+            session.run("MATCH (n:User) RETURN n.username as username LIMIT 25").list().forEach(r ->{
+                System.out.println((r.get("username")));
+            });
+        }
+
     }
 }
