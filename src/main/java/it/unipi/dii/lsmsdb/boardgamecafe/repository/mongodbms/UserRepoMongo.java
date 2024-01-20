a5addc9eea696543ae7665c56b9b0251a14ea663
g.sferr <94866345+g-sferr@users.noreply.github.com>
2024-01-20 21:38:19 +0100

    Class Updated based on GenericUser Type

--- src/main/java/it/unipi/dii/lsmsdb/boardgamecafe/repository/mongodbms/UserRepoMongo.java
+++ src/main/java/it/unipi/dii/lsmsdb/boardgamecafe/repository/mongodbms/UserRepoMongo.java
@@ -1,7 +1,6 @@
 package it.unipi.dii.lsmsdb.boardgamecafe.repository.mongodbms;
 
-//import it.unipi.dii.lsmsdb.phoneworld.model.GenericUser;
-import it.unipi.dii.lsmsdb.boardgamecafe.mvc.model.mongo.UserModelMongo;
+import it.unipi.dii.lsmsdb.boardgamecafe.mvc.model.mongo.GenericUserModelMongo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -10,10 +9,10 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepoMongo extends MongoRepository<UserModelMongo, String> {
+public interface UserRepoMongo extends MongoRepository<GenericUserModelMongo, String> {
 
     @Query("{'username': {$regex : ?0, $options: 'i'}, '_class': ?1}")
-    List<UserModelMongo> findByUsernameRegexAnd_class(String username, String classType);
+    List<GenericUserModelMongo> findByUsernameRegexAnd_class(String username, String classType);
 
-    Optional<UserModelMongo> findByUsername(String username);
+    Optional<GenericUserModelMongo> findByUsername(String username);
 }
