d1830cc5224da35dc50037927b0c1da9a48ab566
g.sferr <94866345+g-sferr@users.noreply.github.com>
2024-01-20 21:35:40 +0100

    Model Classes for Post & Comment added

---
+++ src/main/java/it/unipi/dii/lsmsdb/boardgamecafe/mvc/model/mongo/CommentModelMongo.java
@@ -0,0 +1,74 @@
+package it.unipi.dii.lsmsdb.boardgamecafe.mvc.model.mongo;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+import java.time.Instant;
+
+@Document("comments")
+public class CommentModelMongo {
+    @Id
+    private String id;
+    private String post;
+    private String username;
+    private String text;
+    private Instant timestamp;
+
+    public CommentModelMongo(String id, String post, String username, String text, Instant timestamp) {
+        this.id = id;
+        this.post = post;
+        this.username = username;
+        this.text = text;
+        this.timestamp = timestamp;
+    }
+
+    // GETTERS AND SETTERS
+    public String getId(){
+        return id;
+    }
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public String getPost(){
+        return post;
+    }
+    public void setPost(String post){
+        this.post = post;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getText(){
+        return text;
+    }
+    public void setText(String text){
+        this.text = text;
+    }
+
+    public Instant getTimestamp(){
+        return timestamp;
+    }
+
+    public void setTimestamp(Instant timestamp){
+        this.timestamp = timestamp;
+    }
+
+    // Other
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id='" + id + '\'' +
+                ", post='" + post + '\'' +
+                ", username='" + username + '\'' +
+                ", text=" + text + '\'' +
+                ", timestamp=" + timestamp +
+                '}';
+    }
+}
---
+++ src/main/java/it/unipi/dii/lsmsdb/boardgamecafe/mvc/model/neo4j/CommentModelNeo4j.java
@@ -0,0 +1,4 @@
+package it.unipi.dii.lsmsdb.boardgamecafe.mvc.model.neo4j;
+
+public class CommentModelNeo4j {
+}
