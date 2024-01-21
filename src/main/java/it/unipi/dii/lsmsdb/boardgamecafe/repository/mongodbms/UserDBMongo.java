af696d481c62b544dbe7a50d8a5ffa170984e08b
g.sferr <94866345+g-sferr@users.noreply.github.com>
2024-01-21 22:19:16 +0100

    New Method added in UserDBMongo class in repository package + code reorganization

--- src/main/java/it/unipi/dii/lsmsdb/boardgamecafe/repository/mongodbms/UserDBMongo.java
+++ src/main/java/it/unipi/dii/lsmsdb/boardgamecafe/repository/mongodbms/UserDBMongo.java
@@ -1,20 +1,24 @@
 package it.unipi.dii.lsmsdb.boardgamecafe.repository.mongodbms;
 
-import it.unipi.dii.lsmsdb.boardgamecafe.mvc.model.mongo.GenericUserModelMongo;
+import it.unipi.dii.lsmsdb.boardgamecafe.mvc.model.mongo.*;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
+import java.util.logging.Logger;
 
 @Component
 public class UserDBMongo {
 
-    public UserDBMongo() {
-    }
+    public UserDBMongo() {}
 
-    //private final static Logger logger = LoggerFactory.getLogger(BoardgameModelMongo.class);
+    private final static Logger logger = (Logger) LoggerFactory.getLogger(BoardgameModelMongo.class);
 
     @Autowired
     private UserRepoMongo userRepoMongo;
@@ -35,6 +39,7 @@ public class UserDBMongo {
         }
         return result;
     }
+
     public boolean deleteUser(GenericUserModelMongo user) {
         try {
             userRepoMongo.delete(user);
@@ -73,6 +78,48 @@ public class UserDBMongo {
             return false;
         }
         return true;
+    }
+
+    public boolean updateUser(String id,
+                              GenericUserModelMongo newGenericUser,
+                              String userType) {
+        boolean result = true;
+        try {
+            Optional<GenericUserModelMongo> genericUser = userRepoMongo.findById(id);
+
+            if (genericUser.isPresent())
+            {
+                if (userType.equals("admin")) {
+                    AdminModelMongo administrator = (AdminModelMongo) genericUser.get();
+                    administrator.setUsername(newGenericUser.getUsername());
+                    administrator.setPasswordHashed(newGenericUser.getPasswordHashed());
+                    administrator.setSalt(newGenericUser.getSalt());
+
+                    this.addUser(administrator);
+                } else {
+                    UserModelMongo user = (UserModelMongo) genericUser.get();
+                    UserModelMongo newUser = (UserModelMongo) newGenericUser;
+
+                    user.setUsername(newUser.getUsername());
+                    user.setPasswordHashed(newUser.getPasswordHashed());
+                    user.setSalt(newUser.getSalt());
+                    user.setEmail(newUser.getEmail());
+                    user.setName(newUser.getName());
+                    user.setSurname(newUser.getSurname());
+                    user.setGender(newUser.getGender());
+                    user.setDateOfBirth(newUser.getDateOfBirth());
+                    user.setNationality(newUser.getNationality());
+                    user.setBanned(newUser.isBanned());
+                    user.setReviews(newUser.getReviews());
+
+                    userRepoMongo.save(user);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = false;
+        }
+        return result;
     }
 
 }
