437ef1273862c2c5149810d6a5f56a7a7360b15d
g.sferr <94866345+g-sferr@users.noreply.github.com>
2024-06-24 19:49:39 +0200

    minor fix in initialize method

--- src/main/java/it/unipi/dii/lsmsdb/boardgamecafe/mvc/controller/ControllerViewUserProfilePage.java
+++ src/main/java/it/unipi/dii/lsmsdb/boardgamecafe/mvc/controller/ControllerViewUserProfilePage.java
@@ -4,13 +4,17 @@ import it.unipi.dii.lsmsdb.boardgamecafe.mvc.view.FxmlView;
 import it.unipi.dii.lsmsdb.boardgamecafe.mvc.view.StageManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 @Component
-public class ControllerViewUserHomePage {
+public class ControllerViewUserProfilePage implements Initializable{
 
     @FXML
     private Button profileButton;
@@ -26,20 +30,36 @@ public class ControllerViewUserHomePage {
     private Button searchButton;
     @FXML
     private Button clearFieldButton;
+    @FXML
+    private Button nextButton;
+    @FXML
+    private Button previousButton;
 
     private final StageManager stageManager;
 
     @Autowired
     @Lazy
-    public ControllerViewUserHomePage(StageManager stageManager) {
+    public ControllerViewUserProfilePage(StageManager stageManager) {
         this.stageManager = stageManager;
     }
 
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        this.profileButton.setDisable(true);
+        this.nextButton.visibleProperty().setValue(false);
+        this.previousButton.visibleProperty().setValue(false);
+    }
+
     public void onClickBoardgamePosts(ActionEvent actionEvent) {
+        stageManager.showWindow(FxmlView.REGUSERPOSTS);
+        stageManager.closeStage(this.boardgamePostsButton);
     }
 
     public void onClickBoardgamesColletcion() {
+        stageManager.showWindow(FxmlView.REGUSERBOARDGAMES);
+        stageManager.closeStage(this.boardgamesCollectionButton);
     }
 
     public void onClickYourProfile(ActionEvent actionEvent) {
@@ -59,5 +79,14 @@ public class ControllerViewUserHomePage {
     {
         stageManager.showWindow(FxmlView.WELCOMEPAGE);
         stageManager.closeStage(this.logoutButton);
+    }
+
+    public void onClickSearchUserButton(ActionEvent event) {
+    }
+
+    public void onClickPrevious(ActionEvent event) {
+    }
+
+    public void onClickNext(ActionEvent event) {
     }
 }
