4175c45e3ca855dd2e4b0adef0508269cf716dff
g.sferr <94866345+g-sferr@users.noreply.github.com>
2024-10-22 15:33:15 +0200

    added CustomAggregationOperation calss to perform aggregation operations not directly supported by Spring Data MongoDB operations

---
+++ src/main/java/it/unipi/dii/lsmsdb/boardgamecafe/repository/mongodbms/CustomAggregationOperation.java
@@ -0,0 +1,21 @@
+package it.unipi.dii.lsmsdb.boardgamecafe.repository.mongodbms;
+
+import org.bson.Document;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.data.mongodb.core.aggregation.AggregationOperation;
+import org.springframework.data.mongodb.core.aggregation.AggregationOperationContext;
+
+
+public class CustomAggregationOperation implements AggregationOperation {
+    private final Document operation;
+
+    public CustomAggregationOperation(Document operation) {
+        this.operation = operation;
+    }
+
+    @Override
+    public @NotNull Document toDocument(AggregationOperationContext context) {
+        return context.getMappedObject(operation);
+    }
+}
