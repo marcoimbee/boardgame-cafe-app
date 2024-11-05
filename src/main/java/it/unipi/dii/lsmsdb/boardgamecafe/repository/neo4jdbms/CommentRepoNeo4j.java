ac70bdd5d76a5a2375daac56d4a09b6fecb680ce
g.sferr <94866345+g-sferr@users.noreply.github.com>
2024-11-05 12:32:44 +0100

    Find By Id Update: + Added customized findById method to avid warning

--- src/main/java/it/unipi/dii/lsmsdb/boardgamecafe/repository/neo4jdbms/CommentRepoNeo4j.java
+++ src/main/java/it/unipi/dii/lsmsdb/boardgamecafe/repository/neo4jdbms/CommentRepoNeo4j.java
@@ -1,10 +1,14 @@
 package it.unipi.dii.lsmsdb.boardgamecafe.repository.neo4jdbms;
 
 import it.unipi.dii.lsmsdb.boardgamecafe.mvc.model.neo4j.CommentModelNeo4j;
+import it.unipi.dii.lsmsdb.boardgamecafe.mvc.model.neo4j.UserModelNeo4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.neo4j.repository.query.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CommentRepoNeo4j extends Neo4jRepository<CommentModelNeo4j, String> {
@@ -14,4 +18,8 @@ public interface CommentRepoNeo4j extends Neo4jRepository<CommentModelNeo4j, Str
     void deleteByPost(@Param("postId") String postId);
     @Query("MATCH (c:Comment)<-[:WRITES]-(u:User {username: $username}) DETACH DELETE c")
     void deleteByUsername(@Param("username") String username);
+    @NotNull
+    @Query("MATCH (c: Comment {id: $id}) RETURN c")
+    Optional<CommentModelNeo4j> findById(@Param("id") @NotNull String id);
+
 }
