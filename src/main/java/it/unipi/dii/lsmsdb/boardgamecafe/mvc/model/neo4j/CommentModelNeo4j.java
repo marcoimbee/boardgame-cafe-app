0cc4e159621aea1413ff4196a60de451e9815303
g.sferr <94866345+g-sferr@users.noreply.github.com>
2024-11-05 12:12:29 +0100

    Class Structure Update: + added relationship for comment in neo4j into model design

--- src/main/java/it/unipi/dii/lsmsdb/boardgamecafe/mvc/model/neo4j/CommentModelNeo4j.java
+++ src/main/java/it/unipi/dii/lsmsdb/boardgamecafe/mvc/model/neo4j/CommentModelNeo4j.java
@@ -2,6 +2,7 @@ package it.unipi.dii.lsmsdb.boardgamecafe.mvc.model.neo4j;
 
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Relationship;
 
 
 @Node("Comment")
@@ -9,6 +10,11 @@ public class CommentModelNeo4j {
 
     @Id
     private String id;
+
+    @Relationship(type = "WRITES", direction = Relationship.Direction.INCOMING)
+    private UserModelNeo4j author;
+    @Relationship(type = "REPLY", direction = Relationship.Direction.OUTGOING)
+    private PostModelNeo4j postCommented;
 
     public CommentModelNeo4j() {}
 
@@ -23,6 +29,22 @@ public class CommentModelNeo4j {
     public void setId(String id) {
         this.id = id;
     }
+
+    public UserModelNeo4j getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(UserModelNeo4j author) {
+        this.author = author;
+    }
+
+    public PostModelNeo4j getPostCommented() {
+        return postCommented;
+    }
+    public void setPostCommented(PostModelNeo4j postCommented) {
+        this.postCommented = postCommented;
+    }
+
 
     @Override
     public String toString() {
