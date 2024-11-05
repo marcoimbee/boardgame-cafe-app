eca754f3c739e304af8a4b27d20fb81264175525
g.sferr <94866345+g-sferr@users.noreply.github.com>
2024-11-05 12:13:13 +0100

    Class Structure Update: + post in neo4j into model design for incoming relationship

--- src/main/java/it/unipi/dii/lsmsdb/boardgamecafe/mvc/model/neo4j/PostModelNeo4j.java
+++ src/main/java/it/unipi/dii/lsmsdb/boardgamecafe/mvc/model/neo4j/PostModelNeo4j.java
@@ -1,6 +1,5 @@
 package it.unipi.dii.lsmsdb.boardgamecafe.mvc.model.neo4j;
 
-import it.unipi.dii.lsmsdb.boardgamecafe.mvc.model.mongo.CommentModelMongo;
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
 import org.springframework.data.neo4j.core.schema.Relationship;
@@ -14,14 +13,32 @@ public class PostModelNeo4j {
 
     @Relationship(type = "REPLY", direction = Relationship.Direction.INCOMING)
     private List<CommentModelNeo4j> comments;
-
     @Relationship(type = "REFERS_TO", direction = Relationship.Direction.OUTGOING)
     private BoardgameModelNeo4j taggedGame;
+    @Relationship(type = "WRITES", direction = Relationship.Direction.INCOMING)
+    private UserModelNeo4j author;
+    @Relationship(type = "LIKES", direction = Relationship.Direction.INCOMING)
+    private UserModelNeo4j likes;
 
     public PostModelNeo4j(){}
-
     public PostModelNeo4j(String id) {
         this.id = id;
+    }
+
+    public UserModelNeo4j getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(UserModelNeo4j author) {
+        this.author = author;
+    }
+
+    public UserModelNeo4j getLikes() {
+        return likes;
+    }
+
+    public void setLikes(UserModelNeo4j likes) {
+        this.likes = likes;
     }
 
     public String getId() {
