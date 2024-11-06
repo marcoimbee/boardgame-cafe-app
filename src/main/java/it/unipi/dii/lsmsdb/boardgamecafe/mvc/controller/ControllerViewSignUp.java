a99bb527726869ee32803d0a77ccf97163500bee
g.sferr <94866345+g-sferr@users.noreply.github.com>
2024-11-06 22:52:22 +0100

    File Fxml Updated: + added new methedo and in general fixed the controller for Sign-Up View. +Note: Ther previous commit was combined with others and was done by mistake

---
+++ src/main/java/it/unipi/dii/lsmsdb/boardgamecafe/mvc/controller/ControllerViewSignUp.java
@@ -0,0 +1,360 @@
+package it.unipi.dii.lsmsdb.boardgamecafe.mvc.controller;
+
+import it.unipi.dii.lsmsdb.boardgamecafe.mvc.model.mongo.GenericUserModelMongo;
+import it.unipi.dii.lsmsdb.boardgamecafe.mvc.model.mongo.UserModelMongo;
+import it.unipi.dii.lsmsdb.boardgamecafe.mvc.view.FxmlView;
+import it.unipi.dii.lsmsdb.boardgamecafe.mvc.view.StageManager;
+import it.unipi.dii.lsmsdb.boardgamecafe.repository.mongodbms.UserDBMongo;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Component
+public class ControllerViewSignUp implements Initializable {
+
+    private static final String EMAIL_REGEX =
+            "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+    private static final Pattern pattern = Pattern.compile(EMAIL_REGEX);
+
+    @FXML private Button buttonAdminSignUp;
+    @FXML private TextField textFieldFirstName;
+    @FXML private Label labelFirstName;
+    @FXML private TextField textFieldLastName;
+    @FXML private Label labelLastName;
+    @FXML private ComboBox<String> comboBoxNationality;
+    @FXML private Label labelNationality;
+    @FXML private ComboBox<String> comboBoxGender;
+    @FXML private Label labelGender;
+    @FXML private DatePicker datePickerDate;
+    @FXML private Label labelDate;
+    @FXML private TextField textFieldEmail;
+    @FXML private Label labelEmail;
+    @FXML private TextField textFieldUsername;
+    @FXML private Label labelUsername;
+    @FXML private TextField textFieldPassword;
+    @FXML private Label labelPassword;
+    @FXML private TextField textFieldRepeatPassword;
+    @FXML private Label labelRepeatedPassword;
+    @FXML private Button buttonCancel;
+    @FXML private Button buttonFinish;
+
+    @Autowired
+    private UserDBMongo userDBMongo;
+
+    private final StageManager stageManager;
+
+    private final static Logger logger = LoggerFactory.getLogger(ControllerViewSignUp.class);
+
+    @Autowired @Lazy
+    public ControllerViewSignUp(StageManager stageManager) {
+        this.stageManager = stageManager;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        this.initComboBox();
+
+        //ToDO: Inizializzare tutto ciò che riguarda il sign-up user
+        // per poi disabilitare se click su sign-up admin, così da consentire
+        // l'inserimento solo dei campi che riguardano quest'ultimo
+
+    }
+
+
+    public void onClickCancel(ActionEvent actionEvent)
+    {
+        stageManager.closeStageButton(this.buttonCancel);
+    }
+
+    public void onClickAdminSignUp(ActionEvent actionEvent)
+    {
+        stageManager.closeStageButton(this.buttonAdminSignUp);
+        //stageManager.showWindow(FxmlView.ADMINSIGNUP);
+    }
+
+
+    public void onClickFinish(ActionEvent event) {
+        String firstName = this.textFieldFirstName.getText();
+        String lastName = this.textFieldLastName.getText();
+        String country = this.comboBoxNationality.getValue();
+        String gender = this.comboBoxGender.getValue();
+        LocalDateTime dateOfBirth = selectDate();
+        String email = selectEmail();
+        String username = selectUsername();
+        String password = this.textFieldPassword.getText();
+        String repeatedPassword = this.textFieldRepeatPassword.getText();
+
+        if (firstName.isEmpty()) {
+            labelFirstName.setText("First name is missing.");
+        } else
+        {
+            labelFirstName.setText("");
+        }
+
+        if (lastName.isEmpty()) {
+            labelLastName.setText("Last name is missing.");
+        } else
+        {
+            labelLastName.setText("");
+        }
+
+        if (country == null) {
+            labelNationality.setText("Country is missing.");
+        } else
+        {
+            labelNationality.setText("");
+        }
+
+        if (gender == null) {
+            labelGender.setText("Gender is missing.");
+        } else
+        {
+            labelGender.setText("");
+        }
+
+        // Date of birth converter for service
+        if (dateOfBirth == null) {
+            labelDate.setText("Date of birth is missing.");
+        } else {
+            labelDate.setText("");
+            int year = dateOfBirth.getYear();
+            int month = dateOfBirth.getMonthValue();
+            int day = dateOfBirth.getDayOfMonth();
+        }
+
+        if (email.isEmpty()) {
+            labelEmail.setText("E-mail is missing.");
+        } else if (email.equals("user_banned")) {
+            labelEmail.setText("E-mail already used by banned user.");
+        } else if (email.equals("already_used")) {
+            labelEmail.setText("E-mail already used.");
+        } else if (!validateEmail(email)) {
+            labelEmail.setText("E-mail not valid.");
+            email = null;
+        } else
+        {
+            labelEmail.setText("");
+        }
+
+        if (username.isEmpty()) {
+            labelUsername.setText("Username is missing.");
+        } else if (username.equals("already_used")){
+            labelUsername.setText("Username already exists");
+        } else {
+            labelUsername.setText("");
+        }
+
+        // Password management
+        if (password.isEmpty()) {
+            labelPassword.setText("Password is missing.");
+            labelRepeatedPassword.setText("");
+        } else if (repeatedPassword.isEmpty()) {
+            labelPassword.setText("");
+            labelRepeatedPassword.setText("Repeat the password.");
+        } else if (!password.equals(repeatedPassword)) {
+            labelPassword.setText("");
+            labelRepeatedPassword.setText("Passwords do not match.");
+        } else {
+            labelPassword.setText("");
+            labelRepeatedPassword.setText("");
+        }
+
+
+        String[] campi = {"First Name", "Last Name",
+                          "Country", "Gender", "Date of Birth",
+                          "Email", "Username", "Password", "Repeated Password"};
+
+        List<String> nonNullValues = new ArrayList<>();
+
+        for (String campo : campi) {
+            switch (campo) {
+                case "First Name":
+                    if (firstName != null) nonNullValues.add(firstName);
+                    break;
+                case "Last Name":
+                    if (lastName != null) nonNullValues.add(lastName);
+                    break;
+                case "Country":
+                    if (country != null) nonNullValues.add(country);
+                    break;
+                case "Gender":
+                    if (gender != null) nonNullValues.add(gender);
+                    break;
+                case "Date of Birth":
+                    if (dateOfBirth != null) nonNullValues.add(dateOfBirth.toString());
+                    break;
+                case "Email":
+                    if (email != null) nonNullValues.add(email);
+                    break;
+                case "Username":
+                    if (!username.isEmpty()) nonNullValues.add(username);
+                    break;
+                case "Password":
+                    if (password != null) nonNullValues.add(password);
+                    break;
+                case "Repeated Password":
+                    if (repeatedPassword != null) nonNullValues.add(repeatedPassword);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        // Stampare i valori non nulli
+        System.out.println("Valori non nulli:");
+        for (String value : nonNullValues) {
+            System.out.println(value);
+        }
+
+        // Controllare e stampare le variabili che sono null
+        for (String campo : campi) {
+            switch (campo) {
+                case "First Name":
+                    if (firstName.isEmpty()) System.out.println("First Name è null.");
+                    break;
+                case "Last Name":
+                    if (lastName.isEmpty()) System.out.println("Last Name è null.");
+                    break;
+                case "Country":
+                    if (country == null) System.out.println("Country è null.");
+                    break;
+                case "Gender":
+                    if (gender == null) System.out.println("Gender è null.");
+                    break;
+                case "Date of Birth":
+                    if (dateOfBirth == null) System.out.println("Date of Birth è null.");
+                    break;
+                case "Email":
+                    if (email == null || email.isEmpty()) System.out.println("Email è null.");
+                    break;
+                case "Username":
+                    if (username.isEmpty()) System.out.println("Username è null.");
+                    break;
+                case "Password":
+                    if (password == null || password.isEmpty()) System.out.println("Password è null.");
+                    break;
+                case "Repeated Password":
+                    if (repeatedPassword == null || repeatedPassword.isEmpty()) System.out.println("Repeated Password è null.");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        //ToDo:
+//        stageManager.showInfoMessage("Sign-Up Info: ", "You're Successfully Registered");
+//        stageManager.closeStageButton(this.buttonFinish);
+    }
+
+    public static boolean validateEmail(String email) {
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    public LocalDateTime selectDate() {
+        LocalDate selectedDate = this.datePickerDate.getValue();
+        LocalDate currentDate = LocalDate.now();
+
+        if (selectedDate == null) {
+            labelDate.setText("Date of birth is missing.");
+        } else if (selectedDate.isAfter(currentDate)) {
+            labelDate.setText("Were you born in the future?");
+        } else {
+            LocalDateTime dateTime = selectedDate.atStartOfDay();
+            //String formattedDate = dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"));
+            Period age = Period.between(selectedDate, currentDate);
+
+            if (age.getYears() < 13 ) {
+                labelDate.setText("You must be at least 13 years old.");
+            } else {
+                labelDate.setText("");
+                return dateTime;
+            }
+        }
+        return null;
+    }
+
+    public String selectEmail() {
+        //se email già presente o bannata: messaggio di errore.
+        Optional<GenericUserModelMongo> user = userDBMongo.
+                                               findByEmail(this.textFieldEmail.getText());
+        if (user.isPresent()) {
+            UserModelMongo userFromMongo = (UserModelMongo) user.get();
+            if (userFromMongo.isBanned())
+                return "user_banned";
+            return "already_used";
+        }
+        return this.textFieldEmail.getText();
+    }
+
+
+    public String  selectUsername(){
+
+        //se username già presente: messaggio di errore.
+        Optional<GenericUserModelMongo> user = userDBMongo.
+                                               findByUsername(this.textFieldUsername.getText());
+        if (user.isPresent()) {
+
+            return "already_used";
+        }
+        return this.textFieldUsername.getText();
+    }
+
+    private void initComboBox() {
+        this.comboBoxGender.getItems().addAll("male","female", "not specified");
+        this.comboBoxNationality.getItems().addAll("Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica",
+                "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas",
+                "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan",
+                "Bolivia", "Bonaire Sint Eustatius and Saba", "Bosnia and Herzegovina", "Botswana", "Bouvet Island",
+                "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi",
+                "Cabo Verde", "Cambodia", "Cameroon", "Canada", "Cayman Islands", "Central African Republic", "Chad",
+                "Chile", "China", "Christmas Island", "Cocos Islands", "Colombia", "Comoros", "Congo", "Congo",
+                "Cook Islands", "Costa Rica", "Croatia", "Cuba", "Curaçao", "Cyprus", "Czechia", "Côte d'Ivoire",
+                "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador",
+                "Equatorial Guinea", "Eritrea", "Estonia", "Eswatini", "Ethiopia", "Falkland Islands", "Faroe Islands",
+                "Fiji", "Finland", "France", "French Guiana", "French Polynesia", "French Southern Territories",
+                "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada",
+                "Guadeloupe", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea-Bissau", "Guyana", "Haiti",
+                "Heard Island and McDonald Islands", "Holy See", "Honduras", "Hong Kong", "Hungary", "Iceland",
+                "India", "Indonesia", "Iran", "Iraq", "Ireland", "Isle of Man", "Israel", "Italy", "Jamaica",
+                "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "North Korea", "South Korea",
+                "Kuwait", "Kyrgyzstan", "Lao", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein",
+                "Lithuania", "Luxembourg", "Macao", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta",
+                "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia",
+                "Moldova", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Myanmar",
+                "Namibia", "Nauru", "Nepal", "Netherlands", "New Caledonia", "New Zealand", "Nicaragua", "Niger",
+                "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau",
+                "Palestine State of", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn",
+                "Poland", "Portugal", "Puerto Rico", "Qatar", "Republic of North Macedonia", "Romania",
+                "Russian Federation", "Rwanda", "Réunion", "Saint Barthélemy", "Saint Helena Ascension and Tristan da Cunha",
+                "Saint Kitts and Nevis", "Saint Lucia", "Saint Martin", "Saint Pierre and Miquelon",
+                "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia",
+                "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Sint Maarten", "Slovakia", "Slovenia",
+                "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "South Sudan",
+                "Spain", "Sri Lanka", "Sudan", "Suriname", "Svalbard and Jan Mayen", "Sweden", "Switzerland",
+                "Syrian Arab Republic", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor-Leste", "Togo", "Tokelau",
+                "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands",
+                "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States Minor Outlying Islands",
+                "United States of America", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Viet Nam", "Virgin Islands",
+                "Virgin Islands", "Wallis and Futuna", "Western Sahara", "Yemen", "Zambia", "Zimbabwe", "Åland Islands");
+    }
+}
