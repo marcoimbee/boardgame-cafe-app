1eeda9480dab8e51c8bdeb5e5ceff842e892900e
g.sferr <94866345+g-sferr@users.noreply.github.com>
2024-12-03 17:58:02 +0100

    Constants class: + added constant UPDATE_BOARDGAME to handle update boardgame status by model bean

--- src/main/java/it/unipi/dii/lsmsdb/boardgamecafe/utils/Constants.java
+++ src/main/java/it/unipi/dii/lsmsdb/boardgamecafe/utils/Constants.java
@@ -3,6 +3,7 @@ package it.unipi.dii.lsmsdb.boardgamecafe.utils;
 public class Constants {
     public static final String CURRENT_USER = "currentUser";
     public static final String SELECTED_BOARDGAME = "selectedBoardgame";
+    public static final String UPDATED_BOARDGAME = "updatedBoardgame";
     public static final String SELECTED_USER = "selectedUser" ;
     public static final String SELECTED_STATISTIC = "selectedStatistic";
     public static final String IS_UPDATE = "isUpdate";
