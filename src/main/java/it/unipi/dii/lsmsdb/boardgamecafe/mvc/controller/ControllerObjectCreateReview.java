9acba7d8a7bc63248faed9c538d61534c0f02002
g.sferr <94866345+g-sferr@users.noreply.github.com>
2024-12-03 18:45:36 +0100

    Create/Edit/View Review (controllers+FXMLs): + added review element to manage crate/edit/view operation

---
+++ src/main/java/it/unipi/dii/lsmsdb/boardgamecafe/mvc/controller/ControllerObjectCreateReview.java
@@ -0,0 +1,50 @@
+package it.unipi.dii.lsmsdb.boardgamecafe.mvc.controller;
+
+import it.unipi.dii.lsmsdb.boardgamecafe.mvc.model.mongo.ReviewModelMongo;
+import it.unipi.dii.lsmsdb.boardgamecafe.mvc.view.StageManager;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class ControllerObjectCreateReview {
+
+    @FXML
+    protected Label usernameLabel;
+    @FXML
+    protected Label timestampLabel;
+    @FXML
+    protected Label postLabel;
+    @FXML
+    private Button submitButton;
+    @FXML
+    private Button cancelButton;
+    @FXML
+    protected TextField bodyTextLabel;
+    @FXML
+    private Slider ratingSlider;
+
+    private ReviewModelMongo review;
+    private StageManager stageManager;
+    @Autowired
+    @Lazy
+    public ControllerObjectCreateReview(StageManager stageManager) {
+        this.stageManager = stageManager;
+    }
+
+    public ControllerObjectCreateReview() {}
+
+    public void setData() {}
+
+    @FXML
+    public void onClickSubmitButton() {}
+
+    @FXML
+    public void onClickCancelButton() {}
+}
