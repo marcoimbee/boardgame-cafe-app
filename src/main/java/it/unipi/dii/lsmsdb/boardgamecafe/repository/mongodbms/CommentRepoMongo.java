a6244854e7abeaff87e9599af4809e97540ecbaa
francescoB1997 <francesco.0297@hotmail.it>
2024-12-29 13:13:50 +0100

    CommentRepoMongo commentata

--- src/main/java/it/unipi/dii/lsmsdb/boardgamecafe/repository/mongodbms/CommentRepoMongo.java
+++ src/main/java/it/unipi/dii/lsmsdb/boardgamecafe/repository/mongodbms/CommentRepoMongo.java
@@ -1,18 +1,18 @@
-package it.unipi.dii.lsmsdb.boardgamecafe.repository.mongodbms;
-
-import com.mongodb.client.result.DeleteResult;
-import it.unipi.dii.lsmsdb.boardgamecafe.mvc.model.mongo.CommentModelMongo;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-import org.springframework.stereotype.Repository;
-
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
-
-@Repository
-public interface CommentRepoMongo extends MongoRepository<CommentModelMongo, String> {
-    List<CommentModelMongo> findByUsername(String username);
-    long deleteByPost(String post);
-    void deleteByUsername(String username);
-}
+//package it.unipi.dii.lsmsdb.boardgamecafe.repository.mongodbms;
+//
+//import com.mongodb.client.result.DeleteResult;
+//import it.unipi.dii.lsmsdb.boardgamecafe.mvc.model.mongo.CommentModelMongo;
+//import org.springframework.data.mongodb.repository.MongoRepository;
+//import org.springframework.data.mongodb.repository.Query;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.Date;
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Repository
+//public interface CommentRepoMongo extends MongoRepository<CommentModelMongo, String> {
+//    List<CommentModelMongo> findByUsername(String username);
+//    long deleteByPost(String post);
+//    void deleteByUsername(String username);
+//}
