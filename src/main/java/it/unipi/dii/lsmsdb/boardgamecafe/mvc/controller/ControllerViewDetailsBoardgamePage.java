4b858f810109a6e5fc796337fb488ed350ea64ee
g.sferr <94866345+g-sferr@users.noreply.github.com>
2025-01-17 18:11:12 +0100

    Details Boardgames Page (Controller): + fixed error in update avg rating after edit review operation

--- src/main/java/it/unipi/dii/lsmsdb/boardgamecafe/mvc/controller/ControllerViewDetailsBoardgamePage.java
+++ src/main/java/it/unipi/dii/lsmsdb/boardgamecafe/mvc/controller/ControllerViewDetailsBoardgamePage.java
@@ -252,6 +252,7 @@ public class ControllerViewDetailsBoardgamePage implements Initializable {
     }
 
     private void setAverageRating() {
+
         Double avgRating = boardgame.getAvgRating(); //ControllerViewRegUserBoardgamesPage.getBgameRating(boardgame);
         //if (avgRating == null)
         //    ratingFromTop = ; // reviewMongoOp.getAvgRatingByBoardgameName(boardgame.getBoardgameName());
@@ -370,7 +371,7 @@ public class ControllerViewDetailsBoardgamePage implements Initializable {
         if (updatedReview != null) {
             modelBean.putBean(Constants.UPDATED_REVIEW, null);
             reviews.replaceAll(review -> review.getId().equals(updatedReview.getId()) ? updatedReview : review);
-            //boardgame.getReviews().replaceAll(review -> review.getId().equals(updatedReview.getId()) ? updatedReview : review);
+            boardgame = boardgameDBMongo.findBoardgameById(boardgame.getId()).get();
             fillGridPane();
             setAverageRating();
         }
