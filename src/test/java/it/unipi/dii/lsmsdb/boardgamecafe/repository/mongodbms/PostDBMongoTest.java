07849ba4d5bd446c4e34473d9dbb7f78d7ca6166
marcoimbee <marcoimbelli@yahoo.it>
2025-01-18 18:44:48 +0100

    checked and updated test for mongodb post class

--- src/test/java/it/unipi/dii/lsmsdb/boardgamecafe/repository/mongodbms/PostDBMongoTest.java
+++ src/test/java/it/unipi/dii/lsmsdb/boardgamecafe/repository/mongodbms/PostDBMongoTest.java
@@ -5,9 +5,6 @@ import it.unipi.dii.lsmsdb.boardgamecafe.mvc.model.mongo.PostModelMongo;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,6 +12,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class PostDBMongoTest {
+
     @Autowired
     PostDBMongo postDBMongo;
 
@@ -36,7 +34,8 @@ class PostDBMongoTest {
     @AfterAll
     public static void clean() {}
 
-    @Test @Order(1)
+    @Test
+    @Order(1)
     public void GIVEN_new_posts_WHEN_adding_posts_to_database_THEN_posts_are_saved() {
         post1 = postDBMongo.addPost(post1);
         post2 = postDBMongo.addPost(post2);
@@ -47,7 +46,8 @@ class PostDBMongoTest {
         assertInstanceOf(PostModelMongo.class, post3);
     }
 
-    @Test @Order(10)
+    @Test
+    @Order(10)
     public void GIVEN_existing_post_WHEN_updating_post_text_THEN_text_is_updated() {
         String updatedText = "Updated test";
         post1.setText(updatedText);
@@ -57,7 +57,8 @@ class PostDBMongoTest {
         assertEquals(updatedText, updatedPost.getText());
     }
 
-    @Test @Order(20)
+    @Test
+    @Order(20)
     public void GIVEN_existing_post_WHEN_incrementing_like_count_THEN_like_count_increases() {
         postDBMongo.updateLikeCount(post1.getId(), true);
 
@@ -65,7 +66,8 @@ class PostDBMongoTest {
         assertEquals(1, updatedPost.getLikeCount());
     }
 
-    @Test @Order(30)
+    @Test
+    @Order(30)
     public void GIVEN_posts_with_specific_tag_WHEN_deleting_posts_by_tag_THEN_posts_are_removed() {
         postDBMongo.deleteByTag(testBoardgameName2);
 
@@ -73,7 +75,8 @@ class PostDBMongoTest {
         assertTrue(deletedPost.isEmpty());
     }
 
-    @Test @Order(40)
+    @Test
+    @Order(40)
     public void GIVEN_posts_by_specific_user_WHEN_deleting_posts_by_username_THEN_posts_are_removed() {
         postDBMongo.deleteByUsername(testUsername);
 
@@ -81,7 +84,8 @@ class PostDBMongoTest {
         assertTrue(deletedPost.isEmpty());
     }
 
-    @Test @Order(50)
+    @Test
+    @Order(50)
     public void GIVEN_post_with_comments_WHEN_adding_comment_to_post_THEN_comment_is_added() {
         String testCommentID = "000000000000000000000000";
         comment1 = new CommentModel(testCommentID, testUsername, "test comment text", new Date());
@@ -91,7 +95,8 @@ class PostDBMongoTest {
         assertEquals(testCommentID, updatedPost.getComments().get(0).getId());
     }
 
-    @Test @Order(60)
+    @Test
+    @Order(60)
     public void GIVEN_existing_comment_WHEN_updating_comment_text_THEN_text_is_updated() {
         String updatedCommentText = "updatedCommentText";
         comment1.setText(updatedCommentText);
@@ -101,7 +106,8 @@ class PostDBMongoTest {
         assertEquals(updatedCommentText, updatedComment.getText());
     }
 
-    @Test @Order(70)
+    @Test
+    @Order(70)
     public void GIVEN_post_with_comments_WHEN_deleting_comment_from_post_THEN_comment_is_removed() {
         postDBMongo.deleteCommentFromArrayInPost(post1, comment1);
 
@@ -109,8 +115,33 @@ class PostDBMongoTest {
         assertEquals(0, updatedPost.getComments().size());
     }
 
-    @Test @Order(200)
+    @Test
+    @Order(80)
     public void GIVEN_existing_post_WHEN_deleting_post_THEN_post_is_removed() {
         assertTrue(postDBMongo.deletePost(post1));
+    }
+
+    @Test
+    @Order(90)
+    public void GIVEN_existing_post_WHEN_searching_for_author_username_THEN_post_is_returned() {
+        assertNotNull(postDBMongo.findByUsername(post1.getUsername()));
+    }
+
+    @Test
+    @Order(100)
+    public void GIVEN_existing_posts_WHEN_searching_for_author_username_THEN_recent_posts_are_returned() {
+        assertNotNull(postDBMongo.findRecentPostsByUsername(post1.getUsername(), 10, 0));
+    }
+
+    @Test
+    @Order(110)
+    public void GIVEN_post_with_tag_WHEN_searching_for_tag_THEN_post_is_returned() {
+        assertNotNull(postDBMongo.findByTag(post1.getTag()));
+    }
+
+    @Test
+    @Order(110)
+    public void GIVEN_post_with_tag_WHEN_searching_for_tag_with_limit_and_skip_THEN_post_is_returned() {
+        assertNotNull(postDBMongo.findByTag(post1.getTag(), 10, 0));
     }
 }
