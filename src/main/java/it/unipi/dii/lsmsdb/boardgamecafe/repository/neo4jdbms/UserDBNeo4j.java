49984f67e5c4696ca58d4ef46765d0543a83c251
g.sferr <94866345+g-sferr@users.noreply.github.com>
2025-01-28 15:04:34 +0100

    Cleaning: + updated documentation file and minor fix on pom.xml

--- src/main/java/it/unipi/dii/lsmsdb/boardgamecafe/repository/neo4jdbms/UserDBNeo4j.java
+++ src/main/java/it/unipi/dii/lsmsdb/boardgamecafe/repository/neo4jdbms/UserDBNeo4j.java
@@ -151,11 +151,16 @@ public class UserDBNeo4j {
         return user;
     }
 
-    public List<String> getFollowedUsernamesWhoCreatedAtLeastOnePost(String username, int limitResults, int skipCounter) {
+    public List<String> getFollowedUsernamesWhoCreatedAtLeastOnePost(String username,
+                                                                     int limitResults,
+                                                                     int skipCounter) {
         List<String> followedUsernames = new ArrayList<>();
-        try { followedUsernames = userNeo4jDB.findFollowedUsernamesWhoCreatedAtLeastOnePost(username, limitResults, skipCounter); }
+        try { followedUsernames = userNeo4jDB.findFollowedUsernamesWhoCreatedAtLeastOnePost(username,
+                                                                                            limitResults,
+                                                                                            skipCounter); }
         catch (Exception ex) {
-            System.err.println("[ERROR] getFollowedUsernamesWhoCreatedAtLeastOnePost()@UserDBNeo4j.java raised an exception: " + ex.getMessage());
+            System.err.println("[ERROR] getFollowedUsernamesWhoCreatedAtLeastOnePost()@UserDBNeo4j.java " +
+                    "raised an exception: " + ex.getMessage());
         }
         return followedUsernames;
     }
